806aaca0efcfc5e585ddd97f34e6d93c2e532a31
pmarecki <pmarecki@gmail.com>
2019-04-06 08:40:10 +0200

    Add example tests

--- src/test/java/wsi/various/SolutionXTest.java
+++ src/test/java/wsi/various/SolutionXTest.java
@@ -2,8 +2,14 @@ package wsi.various;
 
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Percentage;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionXTest {
     SolutionX testee;
@@ -22,6 +28,41 @@ public class SolutionXTest {
         int wynik = testee.add(x,y);
 
         //assert
-        Assertions.assertThat(wynik).isEqualTo(23);
+        assertThat(wynik).isEqualTo(23);
     }
+
+  @Test
+  public void shouldAddNegativeNumbers() {
+    int x = -10;
+    int y = -1000;
+    int wynik = testee.add(x,y);
+
+    assertThat(wynik).isEqualTo(x+y);
+  }
+
+  @Test
+  public void shouldComareDoubles() {
+    double x = 1.009;
+    double pi = 1.00;
+
+    assertThat(x).isCloseTo(pi, Percentage.withPercentage(1));
+  }
+
+  @Test
+  public void shouldCompareLists() {
+    List<String> w = Arrays.asList("aa", "bb", "cc");
+
+    assertThat(w).contains("bb");
+    assertThat(w).containsExactlyInAnyOrder("cc", "bb", "aa");
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void expectException() {
+    int x = 10;
+    int y = 0;
+
+    int wynik = x / y;
+  }
+
+  //Mock-owanie ... biblioteka mockito ...
 }
