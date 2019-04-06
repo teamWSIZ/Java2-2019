ebea043ca4a913748c096ae00918c7714f34c627
pmarecki <pmarecki@gmail.com>
2019-04-06 09:16:28 +0200

    Solution of Parenthesis problem

--- src/main/java/wsi/tasks/Parenthesis.java
+++ src/main/java/wsi/tasks/Parenthesis.java
@@ -2,9 +2,14 @@ package wsi.tasks;
 
 public class Parenthesis {
 
+  //założenie: s zawiera tylko '(' albo ')'
   public boolean checkParenthesis(String s) {
-    ///implement me!
-
-    return true;
+    int cnt = 0;
+    for(char c : s.toCharArray()) {
+      if (c=='(') cnt++;
+      else cnt--;
+      if (cnt<0) return false;
+    }
+    return cnt==0;
   }
 }
