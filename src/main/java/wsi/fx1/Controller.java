09e8e3bc1f802efa292f453b9f8bbe848a960d34
pmarecki <pmarecki@gmail.com>
2019-04-13 08:34:04 +0200

    Add menu

--- src/main/java/wsi/fx1/Controller.java
+++ src/main/java/wsi/fx1/Controller.java
@@ -1,9 +1,11 @@
 package wsi.fx1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.util.List;
 
@@ -158,4 +160,11 @@ public class Controller {
     }
 
 
+    public void sayIt(KeyEvent keyEvent) {
+        //stub
+    }
+
+    public void myOpenFile(ActionEvent actionEvent) {
+        //stub
+    }
 }
